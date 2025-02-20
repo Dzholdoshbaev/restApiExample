3fbaa6086cf51bf06a5b5e3614b1fbbb1da83058
abdyrakhman <medzholdoshbaev@gmail.com>
2025-02-20 17:34:48 +0600

    controllers

---
+++ src/main/java/com/example/demo/controller/AuthorController.java
@@ -0,0 +1,71 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.dto.AuthorDto;
+import com.example.demo.exceptions.AuthorNotFoundException;
+import com.example.demo.server.AuthorService;
+import com.example.demo.utils.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/author")
+public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<AuthorDto>> getAllAuthors() {
+        return ResponseEntity.ok(authorService.getAllAuthors());
+    }
+
+    @PostMapping()
+    public ResponseEntity<AuthorDto> create(@RequestBody AuthorDto authorDto) {
+        return ResponseEntity.ok(authorService.createAuthor(authorDto));
+    }
+
+    @DeleteMapping("/{authorId}")
+    public ResponseEntity<?> delete(@PathVariable String authorId) {
+        try {
+            return ResponseEntity.ok(authorService.deleteAuthor(authorId));
+        }catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid UUID format");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (AuthorNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+    }
+
+    @GetMapping("/{authorId}")
+    public ResponseEntity<?> getAuthor(@PathVariable String authorId) {
+        try {
+            return ResponseEntity.ok(authorService.getAuthorById(authorId));
+        }catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid UUID format");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (AuthorNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> edit(@RequestBody  AuthorDto authorDto) {
+        try {
+            return ResponseEntity.ok(authorService.editAuthor(authorDto));
+        }catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid UUID format");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (AuthorNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+
+
+
+import com.example.demo.dto.BookDto;
+import com.example.demo.exceptions.AuthorNotFoundException;
+import com.example.demo.exceptions.BookNotFoundException;
+import com.example.demo.server.BookService;
+import com.example.demo.utils.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @PostMapping()
+    private ResponseEntity<?> createBook(@RequestBody  BookDto bookDto) {
+        try {
+            return ResponseEntity.ok(bookService.createBook(bookDto));
+        }catch (AuthorNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+    }
+
+    @DeleteMapping("/{bookId}")
+    private ResponseEntity<?> deleteBook(@PathVariable String bookId) {
+        try {
+            return ResponseEntity.ok(bookService.deleteBook(bookId));
+        }catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid UUID format");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (BookNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<?> getBook(@PathVariable String bookId) {
+        try {
+            return ResponseEntity.ok(bookService.getBookById(bookId));
+        }catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid UUID format");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (BookNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+    }
+
+    @PutMapping()
+    private ResponseEntity<?> editBook(@RequestBody BookDto bookDto) {
+        try {
+            return ResponseEntity.ok(bookService.editBook(bookDto));
+        }catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid UUID format");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }catch (AuthorNotFoundException e){
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(),e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+    }
+}
