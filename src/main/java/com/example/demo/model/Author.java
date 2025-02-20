5d4e24b3046730559ddf278dceab9cb5343fa30f
abdyrakhman <medzholdoshbaev@gmail.com>
2025-02-20 17:34:19 +0600

    entity for tables

---
+++ src/main/java/com/example/demo/model/Author.java
@@ -0,0 +1,84 @@
+package com.example.demo.model;
+
+
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "uuid")
+    private UUID id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "birthday")
+    private LocalDate birthDate;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "author")
+    private List<Book> bookList;
+
+    public Author() {}
+
+    public Author(UUID id, String name, String surname, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.ToStringExclude;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "uuid")
+    private UUID id;
+    @Column(name = "title")
+    private String title;
+    @ToStringExclude
+    @ManyToOne
+    @JoinColumn(name = "author_uuid")
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(UUID id, String title, Author author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                '}';
+    }
+}
