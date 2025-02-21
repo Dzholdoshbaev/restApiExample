b0b9ff744de1def7ebd2c803fee204b6c10a2c99
abdyrakhman <medzholdoshbaev@gmail.com>
2025-02-21 16:32:46 +0600

    create repositories for authorities and users

---
+++ src/main/java/com/example/demo/repository/AuthoritiesRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Authorities;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface AuthoritiesRepository extends JpaRepository<Authorities, UUID> {
+}
