dc24f99bf375c5e679b5051440d3f4688092787b
abdyrakhman <medzholdoshbaev@gmail.com>
2025-02-21 16:32:36 +0600

    create service for authorities and users

---
+++ src/main/java/com/example/demo/server/impl/AuthoritiesServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.server.impl;
+
+import com.example.demo.server.AuthoritiesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthoritiesServiceImpl implements AuthoritiesService {
+}
---
+++ src/main/java/com/example/demo/server/impl/UsersServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.server.impl;
+
+import com.example.demo.server.UsersService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+}
